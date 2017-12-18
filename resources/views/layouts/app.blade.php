7d398e4c39e51b811e215eda84b786737e9a6964
MitchWijt <mitchel@wijt.net>
2017-12-18 23:33:39 +0100

    created inctructions homepage

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,11 +7,15 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf_token" content="{{ csrf_token() }}" />
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="js.jquery.fadethis.js"></script>
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/general.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/material-design-iconic-font/2.2.0/css/material-design-iconic-font.min.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
     <title>Innocreation</title>
 </head>
 <body>
--- resources/views/public/home.blade.php
+++ resources/views/public/home.blade.php
@@ -14,8 +14,72 @@
         </div>
     </div>
     <div class="homepage-mainContent hidden">
-        <h1>  fhfhfhfh</h1>
-        {{--CONTENT HERE--}}
+        <div class="instructions">
+            <div class="container">
+                <div class="sub-title-container p-t-20">
+                    <h1 class="sub-title">How it works</h1>
+                </div>
+                <div class="hr"></div>
+                <div class="row instructions-flex">
+                    <div class="circle-instructions">
+                        <i style="font-size: 50px; color: #C9CCCF; padding-left: 1px;" class="zmdi zmdi-accounts-outline social-icon-instructions"></i>
+                    </div>
+                    <p class="instructions-text m-0">Create an account with your expertise(s)</p>
+                </div>
+                <div class="row instructions-flex-right">
+                    <p class="instructions-text m-b-0 m-r-20">Have an idea or product you want to develop</p>
+                    <div class="circle-instructions">
+                        <i style="font-size: 50px; color: #C9CCCF; padding-top: 9px; padding-left: 3px;" class="zmdi zmdi-developer-board social-icon-instructions"></i>
+                    </div>
+                </div>
+                <div class="row instructions-flex">
+                    <div class="circle-instructions">
+                        <i style="font-size: 50px; color: #C9CCCF; padding-left: 1px;" class="zmdi zmdi-pin-drop social-icon-instructions"></i>
+                    </div>
+                    <p class="instructions-text m-0">Offer yourself as a service or ask for a service</p>
+                </div>
+                <div class="row instructions-flex-right p-b-20">
+                    <p class="instructions-text m-b-0 m-r-20">Create or join a team and enjoy working together!</p>
+                    <div class="circle-instructions">
+                        <i style="font-size: 50px; color: #C9CCCF; padding-top: 12px; padding-left: 3px;" class="zmdi zmdi-badge-check social-icon-instructions"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="instructions-second ">
+            <div class="container">
+                <div class="row instructions-flex p-t-20">
+                    <div class="circle-instructions-second">
+                        <i style="font-size: 50px; padding-left: 1px;" class="material-icons social-icon-instructions">lightbulb_outline</i>
+                    </div>
+                    <p class="instructions-second-text m-0">Experience other people their ideas. And discuss ideas in the forum</p>
+                </div>
+                <div class="row instructions-flex-right ">
+                    <p class="instructions-second-text m-b-0 m-r-20">Communicate easily with your team through the chat system</p>
+                    <div class="circle-instructions-second">
+                        <i style="font-size: 50px; padding-left: 1px; padding-top: 10px;" class="material-icons social-icon-instructions">chat</i>
+                    </div>
+                </div>
+                <div class="row instructions-flex m-t-20">
+                    <div class="circle-instructions-second">
+                        <i style="font-size: 50px; padding-left: 1px;" class="material-icons social-icon-instructions">attach_money</i>
+                    </div>
+                    <p class="instructions-second-text m-0">Pay all your team members with one push on a button</p>
+                </div>
+                <div class="row instructions-flex-right">
+                    <p class="instructions-second-text m-b-0 m-r-20">Sell your own team products in our shop</p>
+                    <div class="circle-instructions-second">
+                        <i style="font-size: 50px; padding-left: 1px; padding-top: 10px;" class="material-icons social-icon-instructions">shopping_basket</i>
+                    </div>
+                </div>
+                <div class="row instructions-flex p-b-20">
+                    <div class="circle-instructions-second">
+                        <i style="font-size: 50px; padding-left: 1px;" class="fa fa-btc social-icon-instructions"></i>
+                    </div>
+                    <p class="instructions-second-text m-0">We support bitcoin and other cryptocurrencies</p>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 
@@ -27,6 +91,10 @@
             $(".home-background-wrapper").css("height","85vh");
             $(".homepage-mainContent").show();
         }
+    });
+    $("#test").on("click",function () {
+
+       console.log($(window).scrollTop());
     });
 </script>
 @endsection
