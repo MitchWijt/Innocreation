44a31a71a435d0125512076df317dd79da804eee
MitchWijt <mitchel@wijt.net>
2018-01-27 00:00:01 +0100

    finished chat system

--- resources/views/public/user/userAccountChats.blade.php
+++ resources/views/public/user/userAccountChats.blade.php
@@ -1,6 +1,6 @@
 @extends("layouts.app")
 @section("content")
-    <div class="d-flex grey-background vh85">
+    <div class="d-flex grey-background vh100">
         @include("includes.userAccount_sidebar")
         <div class="container">
             <div class="sub-title-container p-t-20">
@@ -37,56 +37,63 @@
                     <? $recentChats = [];?>
                     <? if(isset($userMessages)) { ?>
                         <? foreach($userMessages as $userMessage) { ?>
-                            <? if(!in_array($userMessage->receiver_user_id, $recentChats)) { ?>
-                            <? array_push($recentChats, $userMessage->receiver_user_id)?>
-                                <div class="row d-flex js-center">
-                                    <div class="card text-center">
-                                        <div class="card-block chat-card" data-toggle="collapse" href=".collapseExample" aria-controls="collapseExample" aria-expanded="false" data-user-id="<?= $userMessage->receiver_user_id?>">
-                                            <p class="f-z-rem m-b-5 p-0"><?=$userMessage->users->First()->firstname?></p>
+                            <? if($userMessage->sender_user_id == $user_id) { ?>
+                                <? if(!in_array($userMessage->receiver_user_id, $recentChats)) { ?>
+                                <? array_push($recentChats, $userMessage->receiver_user_id)?>
+                                    <div class="row d-flex js-center">
+                                        <div class="card text-center">
+                                            <div class="card-block chat-card d-flex js-around" data-toggle="collapse" href=".collapseExample" aria-controls="collapseExample" aria-expanded="false" data-user-id="<?= $userMessage->receiver_user_id?>">
+                                                <img class="circle chatImage m-0" src="<?=$userMessage->users->First()->getProfilePicture()?>" alt="">
+                                                <p class="f-z-rem m-b-5 p-0"><?=$userMessage->users->First()->firstname?></p>
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                                <div class="collapse collapseExample" id="" data-user-id="<?= $userMessage->receiver_user_id?>">
-                                    <div class="card card-block">
-                                        <form action="/sendMessageUser" method="post">
-                                            <input type="hidden" name="_token" value="<?= csrf_token()?>">
-                                            <input type="hidden" name="receiver_user_id" value="<?=$userMessage->receiver_user_id?>">
-                                            <input type="hidden" name="sender_user_id" value="<?=$user_id?>">
-                                            <div class="col-sm-12">
-                                            <? foreach($userMessage->getMessages($userMessage->receiver_user_id, $user_id) as $message) { ?>
-                                                <? if($message->sender_user_id == $user_id) { ?>
-                                                    <div class="row">
-                                                        <div class="col-sm-4">
-                                                            <span class="pull-left f-12"><?=$message->time_sent?></span>
-                                                        </div>
-                                                        <div class="col-sm-8">
-                                                            <p class="pull-right"><?=$message->message?></p>
-                                                        </div>
-                                                    </div>
-                                                <? } else { ?>
-                                                    <div class="row ">
-                                                        <p class="col-sm-8 pull-left"><?=$message->message?></p>
-                                                        <div class="col-sm-4">
-                                                            <span class="pull-right"><?=$message->time_sent?></span>
-                                                        </div>
-                                                    </div>
+                                    <div class="collapse collapseExample" data-user-id="<?= $userMessage->receiver_user_id?>">
+                                        <div class="card card-block">
+                                            <form action="/sendMessageUser" method="post">
+                                                <input type="hidden" name="_token" value="<?= csrf_token()?>">
+                                                <input type="hidden" name="receiver_user_id" value="<?=$userMessage->receiver_user_id?>">
+                                                <input type="hidden" name="sender_user_id" value="<?=$user_id?>">
+                                                <div class="col-sm-12 o-scroll" style="max-height: 200px;">
+                                                <? foreach($userMessage->getMessages($userMessage->receiver_user_id, $user_id) as $message) { ?>
+                                                    <? if($message->message != null) { ?>
+                                                        <? if($message->sender_user_id == $user_id) { ?>
+                                                            <div class="row messageSent m-b-10 m-l-200 m-r-30 m-t-15">
+                                                                <div class="col-sm-4">
+                                                                    <span class="f-12"><?=$message->time_sent?></span>
+                                                                </div>
+                                                                <div class="col-sm-8">
+                                                                    <p class=""><?=$message->message?></p>
+                                                                </div>
+                                                            </div>
+                                                        <? } else { ?>
+                                                            <div class="row messageReceived m-b-10 m-r-200 m-l-30 m-t-15">
+                                                                <div class="col-sm-8">
+                                                                    <p class=""><?=$message->message?></p>
+                                                                </div>
+                                                                <div class="col-sm-4">
+                                                                    <span class=""><?=$message->time_sent?></span>
+                                                                </div>
+                                                            </div>
+                                                        <? } ?>
+                                                    <? } ?>
                                                 <? } ?>
-                                            <? } ?>
-                                            </div>
-                                            <hr>
-                                            <div class="row m-t-20">
-                                                <div class="col-sm-12 text-center">
-                                                    <textarea name="message" placeholder="Send your message..." class="input col-sm-10" rows="5"></textarea>
                                                 </div>
-                                            </div>
-                                            <div class="row">
-                                                <div class="col-sm-11 m-b-20 m-t-20">
-                                                    <button class="btn btn-inno pull-right">Sent message</button>
+                                                <hr>
+                                                <div class="row m-t-20">
+                                                    <div class="col-sm-12 text-center">
+                                                        <textarea name="message" placeholder="Send your message..." class="input col-sm-10" rows="5"></textarea>
+                                                    </div>
                                                 </div>
-                                            </div>
-                                        </form>
+                                                <div class="row">
+                                                    <div class="col-sm-11 m-b-20 m-t-20">
+                                                        <button class="btn btn-inno pull-right">Sent message</button>
+                                                    </div>
+                                                </div>
+                                            </form>
+                                        </div>
                                     </div>
-                                </div>
+                                <? } ?>
                             <? } ?>
                         <? } ?>
                     <? } ?>
